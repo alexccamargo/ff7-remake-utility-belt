29c14630d51d2b18df32078417ed4c3f62f976d5
Alex <alexccamargo@gmail.com>
2020-04-24 08:25:31 -0400

    Added react pwa

---
+++ ff7-rub/src/components/Characters.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Characters.css';
+
+function Characters() {
+  return (
+    <div className="characters">
+              <div>SOMETING IS HERE</div>
+    </div>
+  );
+}
+
+export default Characters;
