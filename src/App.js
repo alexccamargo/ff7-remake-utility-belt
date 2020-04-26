3698c6e138807584c4ecb65489a78a34532a4c34
Alex <alexccamargo@gmail.com>
2020-04-26 18:19:48 -0400

    react base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Final Fantasy 7 Remake Utility Belt
+      </header>
+    </div>
+  );
+}
+
+export default App;
